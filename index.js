93d39d79e0e5d4fe0e141089c3ec4f7cf95a99ac
David Yarzebinski <dyarzebinski97@gmail.com>
2023-08-13 17:35:53 -0400

    initial commit and running

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`We have lift off at http://localhost:${port}`);
+});
