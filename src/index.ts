1b421bdedebdcfbd6648cca09c42a2eb28882bad
David Yarzebinski <dyarzebinski97@gmail.com>
2023-08-27 20:04:41 -0400

    change base port

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { SearchController } from "./controllers/search";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(
